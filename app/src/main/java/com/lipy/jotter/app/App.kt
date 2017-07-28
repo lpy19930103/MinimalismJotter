2568409ec58fbb06c413ffce965a13d5ed340f7e
lpy19930103 <504697121@qq.com>
2017-07-28 15:45:54 +0800

    继承有道云笔记sdk

--- app/src/main/java/com/lipy/jotter/app/App.kt
+++ app/src/main/java/com/lipy/jotter/app/App.kt
@@ -2,6 +2,8 @@ package com.lipy.jotter.app
 
 import android.app.Application
 import com.lipy.jotter.dao.DaoService
+import com.youdao.note.sdk.openapi.IYNoteAPI
+import com.youdao.note.sdk.openapi.YNoteAPIFactory
 
 /**
  * Application
@@ -9,12 +11,20 @@ import com.lipy.jotter.dao.DaoService
  */
 class App : Application() {
 
+    var AppId = ""
+     var api :IYNoteAPI? = null
+
     override fun onCreate() {
         super.onCreate()
         if (instances == null) {
             instances = this
         }
         DaoService.getInstance().initDb(this)
+//        [1]利用申请到的 AppId 生成 api 接口类
+         api = YNoteAPIFactory.getYNoteAPI(this, AppId)
+//        [2]注册 App 到有道云笔记
+        if(!api!!.isRegistered()){ api!!.registerApp();
+        }
     }
 
     companion object {
